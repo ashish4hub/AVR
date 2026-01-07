5aba28d5f12a633b092b6fd4c2597fc91ca0bda7
ashish4hub <ashish.k620045@gmail.com>
2026-01-08 00:47:10 +0530

    Re-enable interrupts and update comments in UART RX
    Re-enable global interrupts in rx_ini function and update comments for clarity.

---
+++ UART_rx_ring_buff.c
@@ -0,0 +1,77 @@
+#define F_CPU 16000000UL
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<string.h>
+#define BAUD 9600
+#define BRC ((F_CPU / (16 * BAUD)) - 1)
+#define rx_buff_size 64
+volatile char rx_buff[rx_buff_size];
+volatile uint8_t rx_head = 0;
+volatile uint8_t rx_tail = 0;
+
+//Configure UART RX
+void rx_ini(void){
+  UCSR0B = (1 << RXCIE0) | (1 << RXEN0);
+  UBRR0H = (BRC >> 8);
+  UBRR0L = BRC;
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+//Interrupt function only read the byte received and store in 'data'
+ISR(USART_RX_vect){
+  char data = UDR0; // Read received byte in data 
+  uint8_t next = (rx_head + 1) % rx_buff_size;
+  
+  //Drop byte is buffer is full
+  if(next != rx_tail){
+    rx_buff[rx_head] = data;
+    rx_head = next;
+  }
+}
+uint8_t uart_rx_avail(void){
+  return (rx_head != rx_tail);
+}
+char uart_get_data(void){
+  if(rx_head == rx_tail) return 0;
+  char data = rx_buff[rx_tail];
+  rx_tail = (rx_tail + 1) % rx_buff_size;
+  return data;
+}
+//Building line 
+#define line_size 32
+char line[line_size];
+uint8_t line_idx = 0;
+uint8_t read_line(void){
+  while(uart_rx_avail()){
+    char c = uart_get_data();
+    if(c == '\n' || c == '\r'){
+      line[line_idx] = '\0'; //end string
+      line_idx = 0;
+      return 1;
+    }
+    if(line_idx < line_size - 1){
+      line[line_idx++] = c;
+    }
+  }
+  return 0;
+}
+int main(void){
+  rx_ini();
+  sei();
+  DDRD |= (1 << PD2); // Pin set as output
+  while(1){
+    if(read_line()){
+      //Turn on led when "LED ON" command is received
+      if(strcmp(line,"LED ON") == 0){
+        PORTD |= (1 << PD2);
+      }
+      //Turn off led when "LED OFF" command is received 
+      else if(strcmp(line,"LED OFF") == 0){
+        PORTD &= ~(1 << PD2);
+      }
+    }
+  }
+}
+
+// rx_head -> next position ISR will write to
+// rx_tail -> next position main loop will read from
+// head == tail -> Buffer empty
